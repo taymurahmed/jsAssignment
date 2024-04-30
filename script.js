441347dff7f706f76f9a1a3924c47da2e431c3c1
unknown <ahmed.taymur06@gmail.com.com>
2024-04-30 18:12:18 +0600

    js assignment

---
+++ script.js
@@ -0,0 +1,35 @@
+// 1.How to connect Js and Html? Connect Them.
+// see the html.index file
+// <body>
+  
+//   <script src="./script.js"></script>
+// </body>
+
+
+// 2.How to Write Variables in Javascript? Write at least 3 Variables
+// var a = 20;
+// let f_name = "Taymur";
+// const pi = 3.1416;
+
+
+// 3.What is the difference between let, var, and const variables?
+// var accepts duplicate variables.
+// let doesn't accepts duplicate variables.
+// const doesn't accepts duplicate variables and can be assign value only once.
+
+
+// 4.Suppose you want to print your name on the console. How do you write that?
+// let f_name = "Taymur Ahmed";
+// console.log(f_name);
+
+
+// 5.Suppose you want to print your name on the alert box. How do you write that?
+// let f_name = "Taymur Ahmed";
+// alert(f_name);
+
+
+// 6.Take- two variables, the first number, and the second number, and then print their sum on the console.
+// let first_number = 6;
+// let second_number = 3;
+// let sum = first_number + second_number;
+// console.log("Sum of " + first_number + " + " + second_number + " = " + sum);
